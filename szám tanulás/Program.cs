32963f1e34da01b11b225b15d9f94a21da2fb1d8
Vadgoblin PC <Vadgoblin@github.com>
2018-09-26 20:07:09 +0200

    százasok mán jók, csak a 20<x<100 van gond. In nutshell the problem is the " und\

--- szám tanulás/Program.cs
+++ szám tanulás/Program.cs
@@ -9,7 +9,7 @@ namespace szám_tanulás
     class Program
     {
         static void Main(string[] args)
-        {
+        {/*
             int last = 0;
             Random r = new Random();
             while (true)
@@ -28,57 +28,51 @@ namespace szám_tanulás
                     Console.WriteLine("Rossz! "+ get_string(num));
                 Console.ReadLine();
                 Console.Clear();
-            }
+            }*/
+            Console.WriteLine(get_string(222));
+            Console.WriteLine(get_string(12));
+            Console.ReadLine();
         }
         static string get_string(int num)
         {
             string correct = "";
-            /*
-            switch (num)
-            {
-                case 1:
-                    correct = "eins";
-                    break;
-                case 2:
-                    correct = "zwei";
-                    break;
-                case 3:
-                    correct = "drei";
-                    break;
-                case 4:
-                    correct = "vier";
-                    break;
+            //if (num > 12) throw new Exception();
+            if (num.ToString().Length > 3) throw new Exception();
+            #region száz
+            if (num.ToString().Length>2)switch(Convert.ToInt32(num.ToString()[num.ToString().Length-3]+""))
+                {
+                    case 1:
+                        correct += "einhundert";
+                        break;
+                    case 2:
+                        correct += "zweihundert";
+                        break;
+                    case 3:
+                        correct += "dreihundert";
+                        break;
+                    case 4:
+                        correct += "vierhundert";
+                        break;
+                    case 5:
+                        correct += "fünfhundert";
+                        break;
+                    case 6:
+                        correct += "sechshundert";
+                        break;
+                    case 7:
+                        correct += "siebenhundert";
+                        break;
+                    case 8:
+                        correct += "achthundert";
+                        break;
+                    case 9:
+                        correct += "neunhundert";
+                        break;
+                }
 
-                case 5:
-                    correct = "fünf";
-                    break;
-                case 6:
-                    correct = "sechs";
-                    break;
-                case 7:
-                    correct = "sieben";
-                    break;
-                case 8:
-                    correct = "acht";
-                    break;
-                case 9:
-                    correct = "neun";
-                    break;
-                case 10:
-                    correct = "zehn";
-                    break;
-                case 11:
-                    correct = "elf";
-                    break;
-                case 12:
-                    correct = "zwölf";
-                    break;
-            }
-            */
-            if (num > 12) throw new Exception();
-
-
-            if(num<20)
+            #endregion
+            #region tíz és egy
+            if (num < 10)
             {
                 switch (num)
                 {
@@ -110,38 +104,87 @@ namespace szám_tanulás
                     case 9:
                         correct += "neun";
                         break;
-                    case 10:
-                        correct += "zehn";
-                        break;
-                    case 11:
-                        correct += "elf";
-                        break;
-                    case 12:
-                        correct += "zwölf";
-                        break;
-                    case 13:
-                        correct += "dreizehn";
-                        break;
-                    case 14:
-                        correct += "vierzehn";
-                        break;
-                    case 15:
-                        correct += "fünfzehn";
-                        break;
-                    case 16:
-                        correct += "sechzehn";
-                        break;
-                    case 17:
-                        correct += "siebzehn";
-                        break;
-                    case 18:
-                        correct += "achtzehn";
-                        break;
-                    case 19:
-                        correct += "neunzehn";
-                        break;
                 }
             }
+            else
+            {
+                if (Convert.ToInt32(num.ToString()[num.ToString().Length - 2]+""+ num.ToString()[num.ToString().Length - 1]) < 20)
+                {
+                    switch (num)
+                    {
+                        case 1:
+                            correct += "eins";
+                            break;
+                        case 2:
+                            correct += "zwei";
+                            break;
+                        case 3:
+                            correct += "drei";
+                            break;
+                        case 4:
+                            correct += "vier";
+                            break;
+
+                        case 5:
+                            correct += "fünf";
+                            break;
+                        case 6:
+                            correct += "sechs";
+                            break;
+                        case 7:
+                            correct += "sieben";
+                            break;
+                        case 8:
+                            correct += "acht";
+                            break;
+                        case 9:
+                            correct += "neun";
+                            break;
+                        case 10:
+                            correct += "zehn";
+                            break;
+                        case 11:
+                            correct += "elf";
+                            break;
+                        case 12:
+                            correct += "zwölf";
+                            break;
+                        case 13:
+                            correct += "dreizehn";
+                            break;
+                        case 14:
+                            correct += "vierzehn";
+                            break;
+                        case 15:
+                            correct += "fünfzehn";
+                            break;
+                        case 16:
+                            correct += "sechzehn";
+                            break;
+                        case 17:
+                            correct += "siebzehn";
+                            break;
+                        case 18:
+                            correct += "achtzehn";
+                            break;
+                        case 19:
+                            correct += "neunzehn";
+                            break;
+                    }
+                }
+                else // szám nagyobb vagy egyenéő mint húsz
+                {
+                    if (num.ToString()[num.ToString().Length - 2] != '0')
+                    {
+
+                    }
+                    if (num.ToString()[num.ToString().Length - 1] == '0')
+                    {
+
+                    }
+                }
+            }
+            #endregion
             return correct;
         }
     }
